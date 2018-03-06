074a8b8f55d0a6f8ce4df0f197407cfdcdd6de88
joffre <1163812140@qq.com>
2018-03-06 23:43:45 +0800

    3.6

---
+++ src/main/java/basic/GenericWriting.java
@@ -0,0 +1,43 @@
+package basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Fruit {
+}
+
+class Apple extends Fruit {
+}
+
+
+public class GenericWriting {
+	static List<Apple> apples = new ArrayList<Apple>();
+	static List<Fruit> fruit = new ArrayList<Fruit>();
+	
+	static <T> void writeExact(List<T> list, T item) {
+		list.add(item);
+	}
+	
+	static void f1() {
+		writeExact(apples, new Apple());
+		writeExact(fruit, new Apple());
+		//		writeExact(apples, new Fruit()); //Error
+	}
+	
+	static <T> void writeWithWildcard(List<? super T> list, T item) {
+		list.add(item);
+	}
+	
+	static void f2() {
+		writeWithWildcard(apples, new Apple());
+		writeWithWildcard(fruit, new Apple());
+	}
+	
+	public static void main(String[] args) {
+		f1();
+		f2();
+	}
+}
+
+
