2278016d163d753c82e393b7525424d396f45d6d
joffre <1163812140@qq.com>
2018-05-31 12:13:23 +0800

    2018.5.31

---
+++ src/main/java/hihocoder/Main_1077.java
@@ -0,0 +1,93 @@
+package hihocoder;
+
+import java.util.*;
+
+public class Main_1077 {
+    //线段树，超时
+    private static class Node {
+        int left, right, mid, minW;
+
+        public Node(int left, int right) {
+            this.left = left;
+            this.right = right;
+            this.mid = left + (right - left) / 2;
+        }
+    }
+
+    private static Node[] tree;
+
+    private static int build(int root, int left, int right, int[] weights) {
+        tree[root] = new Node(left, right);
+        if (left == right) {
+            tree[root].minW = weights[left];
+            return tree[root].minW;
+        }
+
+        int mid = tree[root].mid;
+        int curW = Math.min(build(root * 2, left, mid, weights),
+                build(root * 2 + 1, mid + 1, right, weights));
+        tree[root].minW = curW;
+        return curW;
+    }
+
+    private static void update(int root, int index, int w) {
+        if (index == tree[root].left && index == tree[root].right) {
+            tree[root].minW = w;
+            return;
+        }
+        int mid = tree[root].mid;
+        if (index <= mid) {
+            update(root * 2, index, w);
+        } else {
+            update(root * 2 + 1, index, w);
+        }
+        tree[root].minW = Math.min(tree[root * 2].minW, tree[root * 2 + 1].minW);
+    }
+
+    private static int findMin(int root, int left, int right) {
+        int mid = tree[root].mid;
+
+        if (tree[root].left == left && tree[root].right == right) {
+            return tree[root].minW;
+        } else if (right <= mid) {
+            return findMin(root * 2, left, right);
+        } else if (left > mid) {
+            return findMin(root * 2 + 1, left, right);
+        } else {
+            return Math.min(findMin(root * 2, left, mid), findMin(root * 2 + 1, mid + 1, right));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(
+                System.in
+                // "10\n" +
+                //         "3655 5246 8991 5933 7474 7603 6098 6654 2414 884 \n" +
+                //         "6\n" +
+                //         "0 4 9\n" +
+                //         "0 2 10\n" +
+                //         "1 4 7009\n" +
+                //         "0 5 6\n" +
+                //         "1 3 7949\n" +
+                //         "1 3 1227"
+        );
+        int n = in.nextInt(), weight[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            weight[i] = in.nextInt();
+        }
+        tree = new Node[3 * (n + 1)];
+        build(1, 1, n, weight);
+
+        int q = in.nextInt(), op[] = new int[3];
+        for (int i = 0; i < q; i++) {
+            op[0] = in.nextInt();
+            op[1] = in.nextInt();
+            op[2] = in.nextInt();
+            if (op[0] == 0) {
+                System.out.println(findMin(1, op[1], op[2]));
+            } else {
+                update(1, op[1], op[2]);
+            }
+        }
+    }
+}
---
+++ src/main/java/leetcode/problem301/SummaryRanges.java
@@ -0,0 +1,79 @@
+package leetcode.problem301;
+
+import leetcode.util.Interval;
+
+import java.util.*;
+
+public class SummaryRanges {
+    private class BST {
+        int left, right;
+        BST lchild, rchild;
+
+        public BST(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private BST root;
+
+    public SummaryRanges() {
+    }
+
+    private BST delete(BST root, int left, int right) {
+        if (root == null) return null;
+        else if (left > root.right) root.rchild = delete(root.rchild, left, right);
+        else if (right < root.left) root.lchild = delete(root.lchild, left, right);
+        else {
+            if (root.lchild != null) return root.lchild;
+            if (root.rchild != null) return root.rchild;
+            return null;
+        }
+        return root;
+    }
+
+    private BST insert(BST root, int val) {
+        if (root == null) return new BST(val, val);
+        if (val > root.right + 1) root.rchild = insert(root.rchild, val);
+        if (val < root.left - 1) root.lchild = insert(root.lchild, val);
+        if (val == root.left - 1) {
+            root.left = val;
+            if (root.lchild != null) {
+                BST leftMax = root.lchild;
+                while (leftMax.rchild != null) leftMax = leftMax.rchild;
+                if (root.left - leftMax.right <= 1) {
+                    root.left = leftMax.left;
+                    root.lchild = delete(root.lchild, leftMax.left, leftMax.right);
+                }
+            }
+        } else if (val == root.right + 1) {
+            root.right = val;
+            if (root.rchild != null) {
+                BST rightMin = root.rchild;
+                while (rightMin.lchild != null) rightMin = rightMin.lchild;
+                if (rightMin.left - root.right <= 1) {
+                    root.right = rightMin.right;
+                    root.rchild = delete(root.rchild, rightMin.left, rightMin.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    private void inorder(BST root, List<Interval> list) {
+        if (root == null) return;
+        inorder(root.lchild, list);
+        list.add(new Interval(root.left, root.right));
+        inorder(root.rchild, list);
+    }
+
+    public void addNum(int val) {
+        root = insert(root, val);
+    }
+
+    public List<Interval> getIntervals() {
+        List<Interval> list = new ArrayList<>();
+        inorder(root, list);
+        return list;
+    }
+}
